219371d2f78e7377591208c38489fb357f21195a
Diana Park <dianapark411@gmail.com>
2022-03-31 18:02:34 -0300

    Punto 2-a

---
+++ tp2_1_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 20
+
+int main(){
+    int i;
+    int vt[N];
+
+    srand((int)time(NULL));
+
+    for(i = 0; i<N ; i++){
+        vt[i] = 1 + rand()%100;
+        printf(" %d ", vt[i]);
+    } 
+}
